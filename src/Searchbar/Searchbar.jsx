16aa6f12fd08f489abf66c0546885bf6d9961cb8
Olena <kryvtsovaolena@gmail.com>
2022-11-05 21:33:30 +0200

    input

--- src/Searchbar/Searchbar.jsx
+++ src/Searchbar/Searchbar.jsx
@@ -1,16 +1,46 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { SearchBar, Form, Button, Input } from './SearchBar.styled';
-// import { FcSearch } from 'react-icons/fa';
+import { FaSistrix } from 'react-icons/fa';
+
 export class Searchbar extends Component {
+  state = {
+    pictureName: '',
+  };
+
+  handleNameChange = event => {
+    console.log(event.currentTarget.value);
+    this.setState({ pictureName: event.currentTarget.value.toLowerCase() });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.pictureName.trim() === '') {
+      toast.warn('alarm!');
+      return;
+    }
+    this.props.dataForm(this.state.pictureName);
+    this.setState({ pictureName: '' });
+  };
+
   render() {
     return (
       <SearchBar>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Button type="submit">
-            <span>Search</span>
+            <span>
+              <FaSistrix />
+            </span>
           </Button>
 
-          <Input type="text" autocomplite="off" autoFocus />
+          <Input
+            type="text"
+            autocomplite="off"
+            autoFocus
+            value={this.state.pictureName}
+            onChange={this.handleNameChange}
+          />
         </Form>
       </SearchBar>
     );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { fetchPictures } from 'api/fetchPictures';
 import { Searchbar } from 'Searchbar/Searchbar';
+
 export class App extends Component {
+  state = {
+    pictureName: '',
+  };
+  handleFormSubmit = pictureName => {
+    this.setState({ pictureName });
+  };
+
   componentDidMount() {
     fetchPictures('sun').then(console.log);
   }
   render() {
-    return <Searchbar />;
+    return (
+      <div>
+        <Searchbar dataForm={this.handleFormSubmit} />
+        <ToastContainer autoClose={2000} />
+      </div>
+    );
   }
 }
